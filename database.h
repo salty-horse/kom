a1cac9abcb401e91789a8a7ea0d90de7d126fafa
Ori Avtalion <ori@avtalion.name>
2006-08-26 23:45:16 +0300

    documented obj data

--- database.h
+++ database.h
@@ -41,7 +41,7 @@ struct EventLink {
 
 struct Object {
 	Object() : data1(0), type(0), data2(0), proc(0),
-	           data4(0), data5(0), data6(0), isUseImmediate(0), isVisible(0), data8(0),
+	           data4(0), data5(0), isVisible(0), data6(0), isSprite(0), isUseImmediate(0),
 	           data9(0), data10(0), price(0), data11(0), spellCost(0), data12(0),
 	           data13(0), locationType(0), locationId(0), data15(0), data16(0), data17(0),
 	           data18(0) {}
@@ -54,10 +54,10 @@ struct Object {
 	int proc;
 	int data4;
 	int data5;
-	int data6;
-	int isUseImmediate;
 	int isVisible;
-	int data8;
+	int data6;
+	int isSprite;
+	int isUseImmediate;
 	int data9;
 	int data10;
 	int price;
