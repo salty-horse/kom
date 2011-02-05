salty-horse/kom

commit b0c57ad541a089a2e607376402fcb8ebbbe2d1ec

Fail silently when a video isn't found

game.cpp
@@ -1714,6 +1714,13 @@ void Game::doActionPlayVideo(const char *name) {
 				1 - _vm->game()->player()->isNight);
 	}
 
+	// Fail silently when a video isn't found.
+	// For example, when talking to the disco king, DIS00T is
+	// supposed to play, but it doesn't exist
+	if (!Common::File::exists(filename)) {
+		return;
+	}
+
 	_vm->screen()->clearScreen();
 
 	if (pauseAmbient)

commit ecf676668d514bc7d5b764876537849463c0cfcd

Fix FLC video-playing regression
Regression from b0c57ad541a089a2e607376402fcb8ebbbe2d1ec

TODO
@@ -14,6 +14,7 @@ BUGS
 * don't try showing "loading" icon while narrator subtitles are on.
 * narrator subtitles disappear when the audio stops, even before they finish scrolling.
 * room 34 -- "look at" and "use" on the arcade machine doesn't play the "rupert" audio
+* kom/cutsconv/bst/bst01g0.smk seems corrupt - check if original game falls back to playing the .flc
 
 MISSING FEATURES
 ================

game.cpp
@@ -1703,15 +1703,24 @@ void Game::doActionPlayVideo(const char *name) {
 		sprintf(filename, "kom/%s/%s%s%s.smk",
 			dir, prefix, prefix[0] ? "/" : "", videoName.c_str());
 	else {
-		sprintf(filename, "kom/%s/%s%s%s%d.smk",
+		int len = sprintf(filename, "kom/%s/%s%s%s%d.smk",
 			dir, prefix, prefix[0] ? "/" : "", videoName.c_str(),
 			_vm->game()->player()->isNight);
 
-		// If the file doesn't exist, try the other day mode
-		if (!Common::File::exists(filename))
-			sprintf(filename, "kom/%s/%s%s%s%d.smk",
-				dir, prefix, prefix[0] ? "/" : "", videoName.c_str(),
-				1 - _vm->game()->player()->isNight);
+		// If the file isn't found, try the other day mode
+		if (!Common::File::exists(filename)) {
+
+			filename[len-5] = '0' + '1' - filename[len-5];
+
+			if (!Common::File::exists(filename)) {
+
+				// Still not found. Try flc extension
+				filename[len-5] = '0';
+				filename[len-3] = 'f';
+				filename[len-2] = 'l';
+				filename[len-1] = 'c';
+			}
+		}
 	}
 
 	// Fail silently when a video isn't found.

video_player.cpp
@@ -65,27 +65,32 @@ bool VideoPlayer::playVideo(char *filename) {
 	_skipVideo = false;
 	byte backupPalette[256 * 4];
 
-	// Figure out which player to use, based on extension
-	_player = &_smk;
-
 	// Backup the palette
 	_vm->_system->grabPalette(backupPalette, 0, 256);
 
-	if (!_player->loadFile(filename)) {
-		ColorSet *cs;
-		int length = strlen(filename);
-		filename[length - 5] = '0';
-		filename[length - 3] = 'f';
-		filename[length - 2] = 'l';
-		filename[length - 1] = 'c';
-		_player = &_flic;
+	// Figure out which player to use, based on extension
+	int length = strlen(filename);
+
+	if (filename[length - 3] == 's' &&
+		filename[length - 2] == 'm' &&
+		filename[length - 1] == 'k') {
+
+		_player = &_smk;
+
 		if (!_player->loadFile(filename))
-			error("Could not load flic file: %s\n", filename);
+			error("Could not load video file: %s\n", filename);
+
+	// Flic files have associated color set and audio files
+	} else {
+		_player = &_flic;
+
+		if (!_player->loadFile(filename))
+			error("Could not load video file: %s\n", filename);
 
 		filename[length - 3] = 'c';
 		filename[length - 2] = 'l';
 		filename[length - 1] = '\0';
-		cs = new ColorSet(filename);
+		ColorSet *cs = new ColorSet(filename);
 		_vm->screen()->useColorSet(cs, 0);
 		delete cs;
 